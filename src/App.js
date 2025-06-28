7a2d361e2dd3016dc23a5df95776c3ec0f679d9e
graza4645 <graza4645@gmail.com>
2025-06-28 12:34:43 +0530

    C:/Users/NoushadA/AppData/Local/Programs/Git/

--- src/App.js
+++ src/App.js
@@ -4,7 +4,10 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>this code deploying the application</h1>
+      <h1>List of TeamMembers</h1>
+      <h1>MD NOUSHAD ANSARI</h1>
+      <h1>SEKH</h1>
+      <h1>Nayyar</h1>
     </div>
   );
 }
