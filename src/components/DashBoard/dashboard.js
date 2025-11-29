0d154984f3e8d5f506ee4875e3bfff9a454d4710
itnoushad3-boop <itnoushad3@gmail.com>
2025-11-29 05:46:48 +0530

    C:/Users/NoushadA/AppData/Local/Programs/Git/

--- src/components/DashBoard/dashboard.js
+++ src/components/DashBoard/dashboard.js
@@ -1,17 +1,3 @@
-// import React from 'react';
-// import dashboardImage from './new.jpg'; // ✅ correct relative path
-
-// export default function Dashboard() {
-//   return (
-//     <img
-//       src={dashboardImage}
-//       alt="Dashboard"
-//       style={{ width: '100%' , height : '100%' , overflow : 'hidden' }}
-//     />
-//   );
-// }
-
-
 import React from 'react';
 import dashboardImage from './new.jpg'; // ✅ correct relative path
 
---
+++ src/components/Hostel/Hostel_Room/hostelroom.js
@@ -0,0 +1,13 @@
+
+
+const HostelRoom = () =>{
+    return (
+        <div>
+            <h1>Hostel Room</h1>
+        </div>
+    )
+}
+
+
+
+export default HostelRoom;
